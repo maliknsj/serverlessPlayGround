7b254c68e87ba2e8dd9e85cadb307f6d58337d59
maliknsj <maliknsj@gmail.com>
2022-02-06 14:34:03 +0500

    cleaned the structure a little bit

--- serverless/src/functions/hello/index.js
+++ serverless/src/functions/hello/index.js
@@ -1,20 +1,71 @@
-import runWarm from '../../utils/run-warm';
+'use strict'
+const sql = require('mssql');
 
-// Lambda handler. We are using an async function to simplify the code and
-// remove the need to use a callback.
-export const hello = async function hello(event) {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-      version: process.version,
-    }),
-  };
-
-  return response;
+// Take DB Config from environment variables set in Lambda config
+const config = {
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  server: process.env.DATABASE_SERVER,
+  database: process.env.DATABASE_NAME,
+  options: {
+    encrypt: false, // Use this if you're on Windows Azure
+    enableArithAbort: false
+  },
 };
 
-// Export the hander wrapped in the "run warm" utility which will handle events
-// from the scheduler, keeping our actual handler logic clean.
-export default runWarm(hello);
+const response = {
+  statusCode: 200,
+  headers: {
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS,POST,GET,PATCH,PUT',
+    'Set-Cookie': 'XSRF-TOKEN=SOMERANDOMSTRING'
+  },
+  body: 'success',
+};
+
+module.exports.hello = async () => {
+  try {
+
+    const config = {
+      user: 'Admin_Dev',
+      password: 'SPS$My#Health.Chart3Dev;',
+      server: 'sqlserver2019-myhealthchart-dev.cypy9g8vfaq6.eu-west-2.rds.amazonaws.com',
+      database: 'MyHealthChartDev',
+      options: {
+        encrypt: false, // Use this if you're on Windows Azure
+        enableArithAbort: false
+      },
+    };
+    // Open DB Connection
+    const pool = await sql.connect(config);
+
+    // Query Database
+    const request = await pool.request();
+
+    const result = await request.execute('GetGenders');
+
+    // Close DB Connection
+    pool.close();
+
+    // The results of our query
+    //console.log('Results:', result.recordset);
+    const responseObject = {
+      Records: result.recordset,
+    }
+
+    // Use callback if you need to return values from your lambda function.
+    response.statusCode = 200;
+    response.body = JSON.stringify(responseObject);
+    console.log(response);
+    return response;
+  } catch (err) {
+    // Error running our SQL Query
+    console.error('ERROR: Exception thrown running SQL', err);
+    response.statusCode = 408;
+    response.body = 'Something went wrong!';
+    return response;
+  }
+};
+
+
